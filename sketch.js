387ab4b4fcb6fe4dac2b4bb39e979810a7f3af94
Sayen Padilla <antonia.padilla.o@ug.uchile.cl>
2025-09-01 18:11:35 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -19,7 +19,7 @@ const trabajos = [
     },
     {
         id: 4,
-        photo: "https://raw.githubusercontent.com/evorakie/04-herramientas-computacionales/refs/heads/main/img/Lesbian.webpp",
+        photo: "https://raw.githubusercontent.com/evorakie/04-herramientas-computacionales/refs/heads/main/img/Lesbian.webp",
         title: "The Lesbian Avengers - Infografía + Fanzine + Objeto sobre Movimientos Sociales",
         description: "Proyecto III, con Prof. Clarisa Menteguiaga"
     }
